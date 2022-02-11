ba32ed0302c8be041f5c4fb995882033308a90f4
Lucas Sawade <lsawade@princeton.edu>
2022-02-11 14:48:32 -0500

    Fixed on Traverse

--- batch.sh
+++ batch.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 #SBATCH -t00:05:00
 #SBATCH -N 2
-#SBATCH --gpus 8
-#SBATCH -n 8
+#SBATCH -n 64
+#SBATCH --cpus-per-task=4
+#SBATCH --ntasks-per-core=1
+# # SBATCH --gpus-per-task=1
 #SBATCH --output=mixed_gpu.txt
 #SBATCH --reservation=test
+#SBATCH --gres=gpu:4
+# # SBATCH --gpus-per-node=4
+
 
 # The above request will ask for 3 nodes on Traverse
 # because each node has 4 gpus (12 / 4 = 3)
@@ -23,8 +28,16 @@ my_srun() {
     # $1 is hosfile
     # $2 is task/gpu count
     export SLURM_HOSTFILE="$1"
-    srun -n $2 --gpus=$2 --cpus-per-task=1 --gpus-per-task=1 --distribution=arbitrary --exclusive script.sh 
+    srun --ntasks=$2 --gpus-per-task=1 --cpus-per-task=4 --ntasks-per-core=1 --distribution=arbitrary script.sh 
 }
+
+my_srun_no_host() {
+    # $1 is hosfile
+    # $2 is task/gpu count
+    # export SLURM_HOSTFILE="$1"
+    srun --ntasks=$1 --gpus-per-task=1 --cpus-per-task=4 --ntasks-per-core=1 script.sh 
+}
+# --Ntasks-per-node=2
 
 
 # Writing host lists in cyclicly
@@ -58,5 +71,23 @@ block() {
 
 }
 
-cyclic
-block
+# Writing host lists in cyclicly
+normal_no_host() {    
+     my_srun_no_host 4 > nohost.normal.1.out 2>&1 &
+     my_srun_no_host 4 > nohost.normal.2.out 2>&1 &
+}
+
+
+
+# Writinng host files in block
+block_no_host() {
+     my_srun_no_host 6 > nohost.block.1.out 2>&1 &
+     my_srun_no_host 2 > nohost.block.2.out 2>&1 &
+     my_srun_no_host 2 > nohost.block.3.out 2>&1 &
+     my_srun_no_host 6 > nohost.block.4.out 2>&1 &
+     wait
+
+}
+
+normal_no_host
+block_no_host
